d91d8851b1e7afb682e55e496796a5b28eb08541
Mubarak Awal <awalmubarak6@gmail.com>
2019-11-13 23:39:35 +0000

    add chat messages

---
+++ src/components/messageItem.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const renderIncomingMessage = ()=>{
+    return <View style={styles.container}>
+        <View style={styles.messageContainer}>
+        <Text style={styles.senderName}>Marine Hanaman</Text>
+        <View style={styles.messageBodyContainer}>
+            <Text style={styles.messageBodyText} >React Native is an open-source mobile application framework created by Facebook. It is used to d</Text>
+        </View>
+        </View> 
+    </View>
+}
+
+const renderOutGoingMessage = ()=>{
+    return <View style={[styles.container, {justifyContent: "flex-end"}]}>
+        <View style={styles.outGoingMessageContainer}>
+            <Text style={styles.messageBodyText} > Web and UWP by enabling developers to use</Text>
+        </View>
+    </View>
+}
+
+const MessageItem = ({item})=>{
+    if(item%2==0){
+        return renderIncomingMessage()
+    }
+    return renderOutGoingMessage()
+}
+
+styles = StyleSheet.create({
+    container:{
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        marginVertical: 10,
+        marginHorizontal: 10
+    }, 
+
+    messageContainer:{
+        flex: 0.7,
+        alignItems: "center",
+        
+    },
+    outGoingMessageContainer:{
+        flex: 0.7,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#067b7a",
+        borderBottomLeftRadius: 10,
+        borderTopLeftRadius: 10,
+    },
+    
+    messageBodyText:{
+        fontSize: 16,
+        lineHeight: 20
+    },
+    messageBodyContainer:{
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#bdbdbd",
+        borderBottomRightRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    senderName:{
+        width: "100%",
+        borderBottomRightRadius: 4,
+        borderTopRightRadius: 4,
+        marginBottom: 5,
+        fontSize: 16,
+        fontWeight: "bold",
+        color:"#067b7a"
+    }
+    
+    
+})
+
+export default MessageItem;
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {View, Text, StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native'
-import MeetingItem from '../components/meetingItem'
+import MessageItem from '../components/messageItem'
 import SendMessage from '../components/sendMesage'
 
-const ChatScreen = ()=>{
+const ChatScreen = ({navigation})=>{
     return <View style={styles.container}>
         <View style={styles.header}>
             <Text style={styles.headerText} numberOfLines={1}>Second Meeting of Month 5</Text>
@@ -11,9 +11,7 @@ const ChatScreen = ()=>{
         <View style={styles.mainContainer}>
         <FlatList 
             data={[1,2,3,4,5,6,7,8,9,11,10,12,13,14,15,16,17,18]}
-            renderItem={({item})=> (<TouchableOpacity onPress={()=>navigation.navigate("Chat")}>
-            <MeetingItem/>
-        </TouchableOpacity>)}
+            renderItem={({item})=> (<MessageItem item={item}/>)}
             keyExtractor={item => item.toString()}
             ListFooterComponent={<View style={{marginBottom: 200}}/>}
             showsVerticalScrollIndicator={false}
