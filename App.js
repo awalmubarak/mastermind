4defb82ce3919f5c2ed2e299fac3150bdf7f4db1
Mubarak Awal <awalmubarak6@gmail.com>
2019-11-26 07:26:57 +0000

    add sentry error tracking

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ import { DropDownHolder } from './src/commons/DropDownHolder';
 import NavigationService from './src/navigation/NavigationService';
 import { UserProvider } from './src/contexts/UserContext';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({ 
+  dsn: 'https://5ab3a546db58434b965a85db553cf8d3@sentry.io/1834556', 
+});
+
 
 export default function App(){
   
